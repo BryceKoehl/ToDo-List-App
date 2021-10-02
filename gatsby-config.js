0292aef9649bac8ff2215e8308f8463cb902fb49
BryceKoehl <63129946+BryceKoehl@users.noreply.github.com>
2021-10-02 17:17:09 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,8 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://in-info-web4.informatics.iupui.edu/~bkkoehl/n322/",
     title: "ToDo List App",
   },
   plugins: [],
+  pathPrefix: '/~bkkoehl/n322/to-do-list-app',
 };
